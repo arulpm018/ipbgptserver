fa32ed3d850a058af7b92c80a10d0f4bf1a1c8c2
arulpm018 <72654003+arulpm018@users.noreply.github.com>
2024-12-11 13:02:36 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo mkdir -p model
 sudo mkdir -p vector_store
 sudo gsutil -m cp -r gs://ipbgptbucket/model/* model/
@@ -8,6 +7,8 @@ sudo gsutil -m cp -r gs://ipbgptbucket/vector_store/* vector_store/
 
 sudo git clone https://github.com/arulpm018/ipbgptserver.git
 pip install -r requirements.txt
+export HF_KEY=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
 
-# Start the FastAPI application
+cd ipbgptserver
+
 exec uvicorn main:app --host 0.0.0.0 --port 8000
